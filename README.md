8f7960465aa462de83f51a362843b1e086da8b9e
John Sirois <john.sirois@gmail.com>
2019-02-06 10:19:25 -0700

    Create a self-contained repro script.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 A minimal repro repo for the problem described
 [here](https://github.com/pantsbuild/pex/issues/664).
 
+To run the repro execute `./scripts/repro.sh`.
---
+++ scripts/repro.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd "$(git rev-parse --show-toplevel)"
+
+curl -SL https://github.com/pantsbuild/pex/releases/download/v1.6.1/pex27 -O
+chmod +x pex27
+./pex27 -vvv -r requirements.txt . -m example -o build/example
+
+echo "Should work:"
+./build/example
+
+pth_file="$(PEX_INTERPRETER=1 ./build/example -c 'import site; print(site.getsitepackages()[0])')/problematic.pth"
+sudo sh -c "echo "${PWD}" > "${pth_file}""
+trap "sudo rm "${pth_file}"" EXIT
+echo "Created "${pth_file}" with contents $(cat "${pth_file}") and now should fail:"
+./build/example
